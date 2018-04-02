af3b46bb61a9a7a78301963938d1b3b58a68cff2
gongsi <971961844@qq.com>
2018-04-02 11:58:34 +0800

    初始化

--- app/src/main/java/com/stu/grd/uipaintdemo/MainActivity.java
+++ app/src/main/java/com/stu/grd/uipaintdemo/MainActivity.java
@@ -14,5 +14,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.all);
+        int AL;
     }
 }
